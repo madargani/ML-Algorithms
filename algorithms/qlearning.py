afea104a10fa8e39ddf2112f6e76eed5ceba249c
Andre Dargani <Mad3913@gmail.com>
2024-06-30 04:51:25 +0000

    Fix q learning algorithm

--- algorithms/qlearning.py
+++ algorithms/qlearning.py
@@ -7,16 +7,16 @@ class agent:
         self.state_space = state_space
         self.action_space = action_space
         self.q_table = np.zeros(state_space + action_space)
-        self.epsilon = 0.5  # exploration factor
+        self.epsilon = 1  # exploration factor
         self.gamma = 1      # discount rate
-        self.lr = 0.01
+        self.lr = 0.1
 
     def __call__(self, state):
         if np.random.rand() < self.epsilon:
-            return np.random.randint(self.action_space)
+            return tuple(np.random.randint(self.action_space))
         index = np.argmax(self.q_table[state])
         return np.unravel_index(index, self.action_space)
     
     def learn(self, state, action, reward, next_state):
-        td_error = reward + self.gamma * np.max(self.q_table[next_state]) -  self.q_table[state][action]
+        td_error = reward + self.gamma * np.max(self.q_table[next_state]) - self.q_table[state][action]
         self.q_table[state][action] += self.lr * td_error
--- cart_pole_qlearning.py
+++ cart_pole_qlearning.py
@@ -2,13 +2,13 @@ import numpy as np
 from algorithms import qlearning, random_policy
 import gymnasium as gym
 
-NUM_EPS = 3
+NUM_EPS = 100
 
 env = gym.make('CartPole-v1')
 
 # pos, vel, ang, angVel
-state_bounds = ((-2.4, 2.4), (-4.8, 4.8), (-.21, .21), (-.42, .42))
-state_buckets = (1, 1, 6, 6)
+state_bounds = ((-2.4, 2.4), (-4.8, 4.8), (-.21, .21), (-1, 1))
+state_buckets = (1, 1, 6, 3)
 # assign observation values to buckets
 def discretize(obs): 
     state = []
@@ -38,7 +38,10 @@ for ep in range(NUM_EPS):
         state = next_state
 
         if terminated or truncated:
-            print(ep_reward)
+            print(int(agent.epsilon * 100), ep_reward)
+            agent.epsilon *= 0.9 # epsilon decay
             break
+
+    # print(agent.q_table[0, 0])
         
 env.close()
